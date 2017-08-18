0b7c613c48aac9e0288321742c1078a520bd544a
uilenyk <30483770+uilenyk@users.noreply.github.com>
2017-08-18 09:50:53 -0700

    Update CoreRegistry.sol

--- CoreRegistry.sol
+++ CoreRegistry.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.13;
 
 contract CoreClusterRegistrar {
     
+    //cluster name -> cluster address
     mapping(string => address) clusters;
 
     function register(address newCluster, string name) returns (bool){
