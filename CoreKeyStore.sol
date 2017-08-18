a3ea644b923c3cf65cca96e2e9be3a7cad5d084c
uilenyk <30483770+uilenyk@users.noreply.github.com>
2017-08-18 09:47:03 -0700

    format witespace

--- CoreKeyStore.sol
+++ CoreKeyStore.sol
@@ -1,75 +1,38 @@
 pragma solidity ^0.4.13;
 
-
-
 contract CoreKeyStores {
-
-
-
     // map of cluster -> account -> keyjson
-
     mapping(address => mapping(address => Account)) keys;
 
-
-
     function keyExists(address cluster, address account)
-
         internal
-
         returns (bool)
-
     {
-
         if(bytes(keys[cluster][account]).length != 0) return true;
-
         return false;
-
     }
-
-
 
     function storeKey (
-
         address cluster,
-
         address account,
-
         string keyjson
-
     )
-
         public
-
     {
-
         require(cluster != account);
-
         require(!keyExists(cluster, account));
-
         keys[cluster][account] = keyjson;
-
     }
-
-
 
     function getKey(address cluster, address account)
-
         public
-
         constant
-
         returns (string)
-
     {
-
         require(cluster != account);
-
         require(keyExists(cluster, account));
-
         return keys[cluster][account];
-
     }
-
 }
 
 /*
